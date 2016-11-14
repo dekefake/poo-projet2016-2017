da58ed97d30acd7b324dc80bacb470dde23ef3fb
Thomas DE SA <desathomas@gmail.com>
2016-11-14 13:21:47 +0100

    L'attribut catégorie doit être prive

--- projet16_17/Arbitre.java
+++ projet16_17/Arbitre.java
@@ -3,7 +3,7 @@ package projet16_17;
 import java.util.Calendar;
 
 public class Arbitre extends Licencie {
-	int categorie;
+	private int categorie;
 	
 	public Arbitre(int licence, String nm, String p, Calendar d, Club c, int cat) throws IllegalArgumentException{
 		super(licence,nm,p,d,c);
